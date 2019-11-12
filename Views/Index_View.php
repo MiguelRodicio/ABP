491bd997a940bfe6b0cd6938e7a23e1bfb4815eb
Miguel Rodicio <mrsuarez@esei.uvigo.es>
2019-11-12 12:12:55 +0100

    register view

--- Views/Index_View.php
+++ Views/Index_View.php
@@ -1,5 +1,5 @@
 <?php
-
+    
     class Index{
 
         function __construct(){
@@ -8,11 +8,98 @@
 
         function render(){
             include_once '../Views/Header_View.php';
-            include_once '..Functions/Authentication.php';
+            include_once '../Functions/Authentication.php';
+            
         }
 
 
     }
+?>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+
+
+<div class="container">
+<br>
+
+
+
+<div class="row justify-content-center">
+<div class="col-md-6">
+<div class="card">
+<header class="card-header">
+	<a href="" class="float-right btn btn-outline-primary mt-1">Login</a>
+	<h4 class="card-title mt-2">Registrarse</h4>
+</header>
+<article class="card-body">
+<form>
+	<div class="form-row">
+		<div class="col form-group">
+			<label>Nombre </label>   
+		  	<input type="text" class="form-control" placeholder="">
+		</div> <!-- form-group end.// -->
+		<div class="col form-group">
+			<label>Apellidos</label>
+		  	<input type="text" class="form-control" placeholder=" ">
+		</div> <!-- form-group end.// -->
+	</div> <!-- form-row end.// -->
+	<div class="form-group">
+		<label>Email</label>
+		<input type="email" class="form-control" placeholder="">
+		<small class="form-text text-muted">We'll never share your email with anyone else.</small>
+	</div> <!-- form-group end.// -->
+	<div class="form-group">
+			<label class="form-check form-check-inline">
+		  <input class="form-check-input" type="radio" name="gender" value="option1">
+		  <span class="form-check-label"> Hombre </span>
+		</label>
+		<label class="form-check form-check-inline">
+		  <input class="form-check-input" type="radio" name="gender" value="option2">
+		  <span class="form-check-label"> Mujer</span>
+		</label>
+	</div> <!-- form-group end.// -->
+	<div class="form-row">
+		<div class="form-group col-md-6">
+		  <label>City</label>
+		  <input type="text" class="form-control">
+		</div> <!-- form-group end.// -->
+		<div class="form-group col-md-6">
+		  <label>Country</label>
+		  <select id="inputState" class="form-control">
+		    <option> Choose...</option>
+		      <option>Uzbekistan</option>
+		      <option>Russia</option>
+		      <option selected="">United States</option>
+		      <option>India</option>
+		      <option>Afganistan</option>
+		  </select>
+		</div> <!-- form-group end.// -->
+	</div> <!-- form-row.// -->
+	<div class="form-group">
+		<label>Crear Contraseña</label>
+	    <input class="form-control" type="password">
+	</div> <!-- form-group end.// -->  
+    <div class="form-group">
+        <button type="submit" class="btn btn-primary btn-block"> Registrar  </button>
+    </div> <!-- form-group// -->      
+    
+</form>
+</article> <!-- card-body end .// -->
+
+</div> <!-- card.// -->
+</div> <!-- col.//-->
+
+</div> <!-- row.//-->
+
+
+</div> 
+<!--container end.//-->
+
+<br><br>
 
 
 
